092271985a9a9e7c2eab1c7a8899dbfc7f487e86
michaelpotter497 <michaelpotter497@gmail.com>
2017-01-29 14:35:41 -0700

    SOOLVED IT!

--- Vending Machine/VendingMachine.cpp
+++ Vending Machine/VendingMachine.cpp
@@ -12,18 +12,40 @@ using namespace std;
 
 VendingMachine::VendingMachine()
 {
-	
+	for (int i = 0; i < PRODUCT_COUNT; i++)
+	{
+		Item emptyItem = Item("NULL" , -1);
+		merchandise[i] = emptyItem;
+	}	
 }
 
 void VendingMachine::printContents()
 {
 	for (int i = 0; i < PRODUCT_COUNT; i++)
 	{
-		cout << "Product Name at location " << i << " :" << merchandise[i].getProductName() << endl;
+		cout << "Product Name at location " << i + 1 << " :" << merchandise[i].getProductName() << endl;
 	}
 }
 
 void VendingMachine::assignProductLocation(Item _item)
 {
-	
+	for (int i = 0; i < PRODUCT_COUNT; i++)
+	{
+		if (merchandise[i].getPrice() == -1)
+		{
+				merchandise[i] = _item;
+				i = PRODUCT_COUNT;
+		}	
+	}	
 }
+
+Item VendingMachine::getItemAtIndex(int index)
+{
+	return merchandise[index];
+}	
+
+void VendingMachine::removeItemAtIndex(int index)
+{
+	Item emptyItem = Item("NULL" , -1);
+	merchandise[index] = emptyItem;
+}	
